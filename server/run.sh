118b77c8be835f35cab358748c164e683ded9ed8
Raisess <raisechif@gmail.com>
2025-05-21 17:41:15 -0300

    chore: created `server` container

---
+++ server/run.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+
+podman build -t ps2-network-server .
+podman run \
+  -p 8080:8080 \
+  -e SOURCE_PATH=/media/source \
+  -e TARGET_PATH=/media/target \
+  -v $(echo $PS2S_SOURCE_PATH):/media/source \
+  -v $(echo $PS2S_TARGET_PATH):/media/target \
+  --name ps2-network-server \
+  ps2-network-server
